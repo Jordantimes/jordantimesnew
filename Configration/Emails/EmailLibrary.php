951062a65fc058759d6b07a255fc8e5dbf21a519
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-09-22 04:22:58 -0700

    Add files via upload

---
+++ Configration/Emails/EmailLibrary.php
@@ -0,0 +1,5 @@
+<?php
+
+define('SendGridKey', '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');
+define('From' , 'karamleam@gmail.com');
+define('EncryptionKey' , 'JordanTimes@PWD');
