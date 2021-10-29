57c6138b4d642c7403ce7d6b59f1dc7d3678244f
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-10-29 21:10:22 +0200

    Update Company.php

--- app/repositorys/Company.php
+++ app/repositorys/Company.php
@@ -1,6 +1,10 @@
 <?php
 
     class CompanyRepo{
+        public function ImageUpload($file_tmp,$file_size,$file_error,$file_type,$file_name,$file_path){
+            return ImageUpload($file_tmp,$file_size,$file_error,$file_type,$file_name,$file_path);
+        }
+
         public function update_profile($Data){
             $Date = date("Y-m-d");
 
@@ -13,5 +17,37 @@
             }
 
             return Update($table,$columns,$expression);
+        }
+
+        public function Createtrip($Data){
+            $Date = date("Y-m-d");
+
+            $trip_values=[
+                $Data["ID"],
+                $Data["name"],
+                $Data["nameAr"],
+                $Data["start_date"],
+                $Data["end_date"],
+                $Data["days"],
+                $Data["nights"],
+                $Data["description"],
+                $Data["descriptionAr"],
+                $Data["breakfast"],
+                $Data["breakfast_price"],
+                $Data["lunch"],
+                $Data["lunch_price"],
+                $Data["dinner"],
+                $Data["dinner_price"],
+                $Data["image"],
+                $Date,
+                $Date,
+                0
+            ];
+
+            $table = "trips";
+            $columns = "company,name,name_ar,start_date,end_date,days,nights,description,description_ar,breakfast,breakfast_price,lunch,lunch_price,dinner,dinner_price,images,created_at,updated_at,is_deleted";
+            $values = NormalCombine($trip_values);
+            
+            return Insert($table,$columns,$values);
         }
     }
