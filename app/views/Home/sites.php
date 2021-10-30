229feb2b896fa24d3251c5ccc927c14264147c13
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-10-30 13:52:05 +0200

    Update sites.php

--- app/views/Home/sites.php
+++ app/views/Home/sites.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo SITENAME?>Sites</title>
+    <title><?php echo SITENAME?> - Sites</title>
     <link rel="stylesheet" href="<?php echo URLROOT."\public\CSS\Base.css";?>">
     <link rel="stylesheet" href="<?php echo URLROOT."\public\CSS\sites.css";?>">
 
