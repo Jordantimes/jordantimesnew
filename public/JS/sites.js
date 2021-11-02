9e839e410891d0adb071328028e90a3e9e3aa999
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-11-02 22:26:15 +0200

    Update sites.js

--- public/JS/sites.js
+++ public/JS/sites.js
@@ -94,10 +94,9 @@ window.addEventListener('DOMContentLoaded', function(){
 
     const site_pop_up = document.querySelector(".pop_up_site_details_container");
         const site = document.querySelectorAll(".site");
-        
             for(let i = 0 ; i < site.length ; ++i){
                 site[i].addEventListener("click" , function(){
-                    open_popUp();
+                    open_popUp(i);
                 } , true);
             }
     
@@ -158,6 +157,9 @@ window.addEventListener('DOMContentLoaded', function(){
 
                         document.querySelector(".end_location_input").value = "";
                         document.querySelector(".end_location_holder").innerHTML = "-";
+
+                        locations_click[selected_location].style.backgroundColor = "transparent";
+                        locations_click[selected_location].style.color = "#000000";   
                     }
                 }
             }
@@ -214,20 +216,37 @@ window.addEventListener('DOMContentLoaded', function(){
         } , true);
 
 
-        function open_popUp(){
-            let site_pop_up_condition = site_pop_up.getAttribute("condition");
+        function open_popUp(i){
+            document.querySelector(".site_name_popedup").innerText = document.querySelectorAll(".site_name")[i].innerText;
+            document.querySelector(".site_price_popedup").innerText = document.querySelectorAll(".site_price")[i].innerText;
+            document.querySelector(".company_picture_popedup").src = document.querySelectorAll(".company_picture")[i].src;
+            document.querySelector(".company_name_popedup").innerText = document.querySelectorAll(".company_name")[i].innerText;
+            document.querySelector(".site_created_at_popup").innerText = document.querySelectorAll(".site_created_at")[i].innerText;
+            document.querySelector(".site_description_popedup").innerText = document.querySelectorAll(".site_description")[i].innerText;
+            document.querySelector(".breakfast_price_popedup").innerText = document.querySelectorAll(".breakfast_price")[i].innerText ? "- Breakfast " + document.querySelectorAll(".breakfast_price")[i].innerText + "USD": "";
+            document.querySelector(".lunch_price_popedup").innerText = document.querySelectorAll(".lunch_price")[i].innerText ? "- Lunch " + document.querySelectorAll(".lunch_price")[i].innerText + "USD": "";
+            document.querySelector(".dinner_price_popedup").innerText = document.querySelectorAll(".dinner_price")[i].innerText ? "- Dinner " + document.querySelectorAll(".dinner_price")[i].innerText + "USD": "";
+            document.querySelector(".site_base_price_popedup").innerText = "- Trip base price "+document.querySelectorAll(".site_base_price")[i].innerText;
+            document.querySelector(".site_start_date_popedup").innerText = document.querySelectorAll(".site_start_date")[i].innerText;
+            document.querySelector(".site_end_date_popedup").innerText = document.querySelectorAll(".site_end_date")[i].innerText;
+            document.querySelector(".check_in").value = document.querySelectorAll(".site")[i].value;
 
-            if(site_pop_up_condition === "hidden"){
-                site_pop_up.style.display= "block";
-                site_pop_up.setAttribute("condition" , "visible");
-                document.querySelector("body").style.overflow="hidden";
+            let images = document.querySelectorAll(".site_image_wrapper")[i].children;
+            let images_container = document.querySelector(".controlled_slider");
+            images_container.innerHTML = "";
+
+            for(let j = 0 ; j < images.length ; ++j){
+                images_container.innerHTML += "<div class='preview_image_container'><img class='preview_image' src='"+images[j].src+"' alt='site picture'></div>"
             }
 
+            let preview_images = document.querySelectorAll(".preview_image");
+            for(let j = 0 ; j < images.length ; ++j){
+                preview_images[j].addEventListener("click" , function(){
+                    open_image_full_preview(images[j].src);
+                } , false);
+            }
 
-
-            let site_images = document.querySelectorAll(".preview_image");
-            let images_count = site_images.length;
-            create_nav_circles(images_count);
+            create_nav_circles(images.length);
 
             const controlled_slider = document.querySelector(".controlled_slider");
             const navigators = document.querySelectorAll(".navigator");
@@ -240,7 +259,7 @@ window.addEventListener('DOMContentLoaded', function(){
                         }
 
                         else if (nav_number === navigators.length - 1){
-                            let position = (images_count * 290) - 870;
+                            let position = (images.length * 290) - 870;
                             controlled_slider.style.transform="translateX(-"+position+"px)";
                         }
 
@@ -257,6 +276,14 @@ window.addEventListener('DOMContentLoaded', function(){
 
                     } , true);
                 }
+
+            let site_pop_up_condition = site_pop_up.getAttribute("condition");
+
+            if(site_pop_up_condition === "hidden"){
+                site_pop_up.style.display= "block";
+                site_pop_up.setAttribute("condition" , "visible");
+                document.querySelector("body").style.overflow="hidden";
+            }
                 
         }
         function close_popUp(){
@@ -278,18 +305,18 @@ window.addEventListener('DOMContentLoaded', function(){
             const slider_navigators = document.querySelector(".slider_navigators");
             slider_navigators.innerHTML="";
 
-            let circle_selected = "<button class='navigator' navigator_number='0'><div class='inner_active_navigator'></div></button>";
+            let circle_selected = "<button class='navigator' navigator_number='0' type='button'><div class='inner_active_navigator'></div></button>";
             slider_navigators.innerHTML+= circle_selected;
 
             for (let i = 1; i < circles_count ; ++i) {
-                let circle = "<button class='navigator' navigator_number="+i+"></button>";
+                let circle = "<button class='navigator' navigator_number="+i+" type='button'></button>";
                 slider_navigators.innerHTML+= circle;
             }
         }
 
-        function open_image_full_preview(full_screen_container,full_screen_image,image_src){
-            full_screen_container = document.querySelector(".pop_up_site_image_full_screen_view_container");
-            full_screen_image = document.querySelector(".full_screen_image");
+        function open_image_full_preview(image_src){
+            let full_screen_container = document.querySelector(".pop_up_site_image_full_screen_view_container");
+            let full_screen_image = document.querySelector(".full_screen_image");
 
             full_screen_container.style.display = "block";
             full_screen_container.setAttribute("condition" , "visible");
