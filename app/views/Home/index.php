10cfe1e155e775009f3000068c4bf0fe22353754
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-11-12 13:25:16 +0200

    Update index.php

--- app/views/Home/index.php
+++ app/views/Home/index.php
@@ -10,6 +10,12 @@
 <body>
     <div class="main_view">
         <?php require_once APPROOT."\Views\INCLUDES\Header.php"; ?>
+
+        <?php if(!empty($data["trip"])){ ?>
+            <div>
+                <a href="<?php echo URLROOT."/Customer/Book?trip=".$data["trip"]."&passengers=".$data["passengers"]?>">Continue</a>
+            </div>
+        <?php } ?>
     </div>
 </body>
 </html>
