74eeaf1dfb8f308278fd2ec59537648fea13d611
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-11-12 13:28:38 +0200

    Update Customer.php

--- app/repositorys/Customer.php
+++ app/repositorys/Customer.php
@@ -18,4 +18,12 @@
             return Update($table,$columns,$expression);
 
         }
+
+        public function book_customer($Data){
+            $table = "booked";
+            $columns = "trip,user,name,phone,age,nationID,created_at,updated_at,is_deleted";
+            $values = NormalCombine($Data);
+
+            return Insert($table,$columns,$values);
+        }
     }
