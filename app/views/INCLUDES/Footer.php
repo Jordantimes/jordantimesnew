a9cbcc07c0b0b596afd47d9767ea552df4af1118
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-12-08 18:39:42 +0200

    Add files via upload

---
+++ app/views/INCLUDES/Footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <div class="links">
+        <a href="<?php echo URLROOT."/Home/About"?>">About us</a>
+        <a href="<?php echo URLROOT."/Home/Contact"?>">Contact</a>
+    </div>
+
+    <p>A graduation project by Omar hatem and sama mansour.</p>
+</div>
