dbd2f42cba76e267bc2e495ab81f1690c108a157
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-12-08 20:48:33 +0200

    Update LogIn.php

--- app/views/User/LogIn.php
+++ app/views/User/LogIn.php
@@ -4,24 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo SITENAME?> - Log in</title>
+    <link rel="stylesheet" href="<?php echo URLROOT."\public\CSS\Base.css";?>">
+    <link rel="stylesheet" href="<?php echo URLROOT."\public\CSS\login.css";?>">
 </head>
 <body>
-    <?php 
-        if(!empty($data["Message"])){
-            echo $data["Message"];
-        }
-    ?>
-
     <div class="main_view">
-        <form action="<?php echo URLROOT; ?>/User/LogIn" method="POST">
-            <input type="email" name="email" placeholder="email">
-            <input type="password" name="password" placeholder="password">
+        <?php require_once APPROOT."\Views\INCLUDES\Header.php"; ?>
 
-            <button type="submit" name="login_Submit">Log in</button>
-        </form>
+        <div class="content">
+            <div class="form_container">
+                <div class="form_header">
+                    <h1>Log in</h1>
+                </div>
 
-        <a href="<?php echo URLROOT; ?>/User/PasswordUpdate">Forgot password?</a>
+                <form action="<?php echo URLROOT; ?>/User/LogIn" method="POST">
+                    <div class="input_wrapper">
+                        <label for="">E-mail</label>
+                        <input type="email" name="email">
+                    </div>
+
+                    <div class="input_wrapper">
+                        <label for="">Password</label>
+                        <input type="password" name="password">
+                    </div>
+
+                    <?php 
+                        if(!empty($data["Message"])){
+                            echo "<p class='error'>".$data["Message"]."</p>";
+                        }
+                    ?>
+
+                    <button type="submit" name="login_Submit" class="submit">Log in</button>
+                </form>
+
+
+                <a href="<?php echo URLROOT; ?>/User/PasswordUpdate">Forgot password?</a>
+            </div>
+        </div>
+
+        <?php require_once APPROOT."\Views\INCLUDES\Footer.php"; ?>
     </div>
 </body>
 </html>
