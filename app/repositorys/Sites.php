719e2164b41dac7ce94d4e85b72c1c90fe53f689
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-12-11 20:21:16 +0200

    Update Sites.php

--- app/repositorys/Sites.php
+++ app/repositorys/Sites.php
@@ -33,6 +33,22 @@
             return $data;
         }
 
+        public function GetAllSites(){
+            $data = [];
+            $table = "trips,users";
+            $columns = "users.name as company_name,image,trips.id,trips.name,name_ar,start_location,end_location,start_date,end_date,
+                        days,nights,description,description_ar,price,breakfast,breakfast_price,lunch,lunch_price,dinner,dinner_price,images,created_at";
+            $expression = "is_deleted = false AND users.id = company";
+
+            $result =  SelectByCondition($table,$columns,$expression);
+            while($row = $result->fetch(PDO::FETCH_ASSOC)){
+                $row["images"] = explode("," , $row["images"]);
+                array_push($data , $row);
+            }
+
+            return $data;
+        }
+
         public function GetSiteByTripID($id){
             $data = null;
             $table = "trips,users";
