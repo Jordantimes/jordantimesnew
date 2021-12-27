4c60f45171e452862dc8799214654e1db7802e81
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-12-27 16:11:36 +0200

    Update Footer.php

--- app/views/INCLUDES/Footer.php
+++ app/views/INCLUDES/Footer.php
@@ -2,6 +2,19 @@
     <div class="links">
         <a href="<?php echo URLROOT."/Home/About"?>">About us</a>
         <a href="<?php echo URLROOT."/Home/Contact"?>">Contact</a>
+
+        <div id="google_translate_element"></div> 
+        <script type="text/javascript"> 
+            function googleTranslateElementInit() {
+                new google.translate.TranslateElement(
+                    {pageLanguage: 'en'}, 
+                    'google_translate_element'
+                ); 
+            } 
+        </script> 
+        
+        <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+        </script> 
     </div>
 
     <p>A graduation project by Omar hatem and sama mansour.</p>
