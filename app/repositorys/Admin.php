c62dadd4392a104ddbfa112dd3e6951a55d09784
Omar Hatem <89255580+Omarhatem2@users.noreply.github.com>
2021-12-31 11:04:45 +0200

    Update Admin.php

--- app/repositorys/Admin.php
+++ app/repositorys/Admin.php
@@ -1,6 +1,21 @@
 <?php
 
     class AdminRepo{
+        public function GetSignUpCountPerMonth(){
+            $data = [];
+
+            $table= "users";
+            $columns = "CreatedAt AS x,count(*) as y";
+            $expression =  "role = 'company' AND isDeleted = false  GROUP BY year(CreatedAt),month(CreatedAt) ORDER BY year(CreatedAt),month(CreatedAt)";
+
+            $result =  SelectByCondition($table,$columns,$expression);
+            while($row = $result->fetch(PDO::FETCH_ASSOC)){
+                array_push($data , $row);
+            }
+
+            return $data;
+        }
+        
         public function AcceptTrip($ID){
             $Date = date("Y-m-d");
 
